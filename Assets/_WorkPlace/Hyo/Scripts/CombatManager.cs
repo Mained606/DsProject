85f89fc760dbc6e26532b1f66ad0462dda30968b
Mained606 <mained606@gmail.com>
2025-01-14 20:14:20 +0900

    컴뱃매니저 기본 구현
    컴뱃 매니저 초기 구현

---
+++ Assets/_WorkPlace/Hyo/Scripts/CombatManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class CombatManager : BaseManager<CombatManager>
+{
+    protected override void HandleGameStateChange(GameSystemState newState, object additionalData)
+    {
+
+    }
+    
+    // 공격 처리 메서드
+    public void ProcessAttack(CharacterData player, CharacterData enemy, Transform enemyTransform, bool isPlayerAttacking)
+    {
+        CharacterData attacker = isPlayerAttacking ? player : enemy;
+        CharacterData defender = isPlayerAttacking ? enemy : player;
+        
+        if (defender == null || defender.currentHp <= 0)
+        {
+            Debug.LogWarning($"{defender.characterName}는 이미 사망했습니다.");
+            return;
+        }
+
+        int damage = CalculateDamage(attacker, defender);
+
+        defender.TakeDamage(damage);
+
+        Debug.Log($"{attacker.characterName}가 {defender.characterName}에게 {damage}의 데미지를 입혔습니다.");
+
+        // 대상이 사망했는지 확인
+        if (defender.currentHp <= 0)
+        {
+            if (isPlayerAttacking)
+            {
+                // 플레이어 사망 처리
+                Debug.LogError("플레이어 사망");
+                return;
+            }
+            HandleDefeated(defender, enemyTransform);
+        }
+    }
+    
+    // 데미지 계산 메서드
+    public int CalculateDamage(CharacterData attacker, CharacterData defender)
+    {
+        // 공격자의 물리 공격력, 방어자의 물리 방어력
+        int damage = attacker.physicalDamage - defender.physicalDefense;
+
+        // 크리티컬 여부 체크
+        bool isCritical = Random.value < attacker.criticalChance;
+        damage = attacker.CalculateDamage(isCritical);
+        
+        // 데미지는 최소 0 이상
+        return Mathf.Max(0, damage);
+    }
+
+    // 몬스터 사망 처리
+    private void HandleDefeated(CharacterData defeatedCharacter, Transform defenderTransform)
+    {
+        // defeatedCharacter를 MonsterData로 캐스팅
+        MonsterData monsterData = defeatedCharacter as MonsterData;
+        if (monsterData != null)
+        {
+            CharacterManager.Instance.OnMonsterDefeated(monsterData, defenderTransform.position);
+        }
+        else
+        {
+            Debug.LogError("defeatedCharacter를 MonsterData로 캐스팅할 수 없습니다.");
+        }
+    }
+}
