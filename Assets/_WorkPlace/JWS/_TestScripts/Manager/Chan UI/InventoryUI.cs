9a60f6bb9ee6a6f3b256b0f2bb5e138d5d3f7103
UnderBridgeKid97 <waktaverse24@gmail.com>
2025-01-20 01:26:54 +0900

    01.20
    인벤토리ui 수정

--- Assets/_WorkPlace/JWS/_TestScripts/Manager/Chan UI/InventoryUI.cs
+++ Assets/_WorkPlace/JWS/_TestScripts/Manager/Chan UI/InventoryUI.cs
@@ -2,19 +2,40 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 public class InventoryUI : MonoBehaviour
 {
     [SerializeField] private GameObject itemPrefab;
     [SerializeField] private Transform itemsParent;
+
+    #region 툴팁 관련
+   
+    #endregion
+
     private Button[] buttons;
     private int currentButtonIndex = 0;
     private Dictionary<string, List<Item>> categorizedItems = new Dictionary<string, List<Item>>();
 
+   
+
     private void Awake()
     {
         buttons = transform.GetComponentsInChildren<Button>();
+        if (buttons != null && buttons.Length > 0)
+        {
+            Debug.Log($"버튼이 {buttons.Length}개 연결되었습니다.");
+        }
+        else
+        {
+            Debug.LogWarning("버튼이 연결되지 않았거나 배열이 비어있습니다.");
+        }
+        #region 툴팁 관련 초기화 추가
+       
+        #endregion
     }
+
+  
 
     private void OnEnable()
     {
@@ -94,9 +115,15 @@ public class InventoryUI : MonoBehaviour
     private void CreateItemUI(Item item)
     {
         var inventoryItem = Instantiate(itemPrefab, itemsParent);
-        inventoryItem.GetComponentInChildren<TextMeshProUGUI>().text = item.ToStringTMPro();
-        if (item.sprite != null ) inventoryItem.GetComponentsInChildren<Image>()[1].sprite = item.sprite;
+        // -> 툴팁으로 따로 빼려고 임시로 주석 처리. 슬롯 자체에서 전부 보여줄거면 살리기
+        // inventoryItem.GetComponentInChildren<TextMeshProUGUI>().text = item.ToStringTMPro();
+        if (item.sprite != null) inventoryItem.GetComponentsInChildren<Image>()[1].sprite = item.sprite;
         Debug.Log(item.ToStringTMPro());
+
+        #region 툴팁 관련 추가부분
+       
+        #endregion
+
     }
 
     public void AddButtonListeners()
@@ -106,6 +133,7 @@ public class InventoryUI : MonoBehaviour
             int index = i;
             buttons[i].onClick.RemoveAllListeners();
             buttons[i].onClick.AddListener(() => OnButtonClick(index));
+
         }
     }
 
