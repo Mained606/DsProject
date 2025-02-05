cb1bf8d3e86fc42c9dcbbfcc8e9984c30ffa99df
ujeon1973 <ujeon1973@gmail.com>
2025-02-05 12:28:43 +0900

    스크립터블 자동저장관리추가
    스크립터블 자동저장관리추가

--- Assets/_WorkPlace/JWS/_TestScripts/Manager/Camera/CameraPoseSetting.cs
+++ Assets/_WorkPlace/JWS/_TestScripts/Manager/Camera/CameraPoseSetting.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CameraPoseSetting", menuName = "Ds Project/CameraPose")]
@@ -6,4 +7,22 @@ public class CameraPoseSetting : ScriptableObject
 {
     public List<CameraPose> poseList;
     public List<CameraPoseList> poseTransitionList;
+    private void OnEnable()
+    {
+        GameManager.RegistAsset(this); // 중앙 관리 등록
+    }
+
+    private void OnDisable()
+    {
+        SaveAsset(); // 자동 저장
+    }
+
+    public void SaveAsset()
+    {
+#if UNITY_EDITOR
+        EditorUtility.SetDirty(this);
+        AssetDatabase.SaveAssets();
+        Debug.Log($"{name} 자동 저장됨.");
+#endif
+    }
 }
