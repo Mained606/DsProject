3e59095eab415d83894fd9b3b3cad1ad412b60f0
ujeon1973 <ujeon1973@gmail.com>
2025-02-05 21:13:19 +0900

    추가 Fixed
    추가 Fixed

--- Assets/_WorkPlace/JWS/_TestScripts/Manager/Chan UI/InventoryUI.cs
+++ Assets/_WorkPlace/JWS/_TestScripts/Manager/Chan UI/InventoryUI.cs
@@ -88,17 +88,17 @@ public class InventoryUI : MonoBehaviour
         }
         switch (currentButtonIndex)
         {
-            //case 7:
-            //    Debug.Log("제거 처리로직 필요");
-            //    break;
-
             case 7:
+                Debug.Log("제거 처리로직 필요");
+                break;
+
+            case 8:
                 GameStateMachine.Instance.ChangeState(GameSystemState.MainMenu);
                 break;
 
-            //case 9:
-            //    Debug.Log("장착 처리로직 필요");
-            //    break;
+            case 9:
+                Debug.Log("장착 처리로직 필요");
+                break;
 
             default:
                 if (currentButtonIndex >= 0 && currentButtonIndex < 7)
