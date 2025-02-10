5f97c4d38b41a5c55dc97566f6489e2dafed0290
iri-sh326 <bf980326@gmail.com>
2025-02-10 13:06:45 +0900

    퀘스트 진행도에 따른 보스 visible
    .

--- Assets/_WorkPlace/JWS/_TestScripts/Spawn/NPCSpawner.cs
+++ Assets/_WorkPlace/JWS/_TestScripts/Spawn/NPCSpawner.cs
@@ -86,8 +86,8 @@ public class NPCSpawner : MonoBehaviour
 
     private void SpawnObjectAction()
     {
-        //if (spawnData.spwanType == SpawnnerType.Boss && QuestManager.CurrentMainQuestIndex != 8)
-        //    return;
+        if (spawnData.spwanType == SpawnnerType.Boss && QuestManager.CurrentMainQuestIndex != 7)
+            return;
         if (!spawnDelayTimer.IsRunning && ActiveObjectCount == 0)
         {
             TimerManager.Instance.StartTimer(spawnDelayTimer);
