bbdaf902d0d58862552d5ecd541de40680df2a51
UnderBridgeKid97 <waktaverse24@gmail.com>
2025-03-05 18:07:52 +0900

    03.05
    중간 저장

--- Assets/_WorkPlace/Chan/Script/UI/TestStatUP.cs
+++ Assets/_WorkPlace/Chan/Script/UI/TestStatUP.cs
@@ -5,29 +5,30 @@ using UnityEngine.UI;
 public class TestStatUP : MonoBehaviour
 {
     #region 
-    /*   [SerializeField] private Transform NamePanel;
-       [SerializeField] private Transform ValuePanel;
+    [SerializeField] private Transform NamePanel;
+    [SerializeField] private Transform ValuePanel;
 
-       private TextMeshProUGUI[] name;
-       private TextMeshProUGUI[] value;
-       private Button[] button;
+    private TextMeshProUGUI[] Statname;
+    private TextMeshProUGUI[] Statvalue;
+    private Button[] button;
 
 
-       void Start()
-       {
-           name = NamePanel.GetComponentsInChildren<TextMeshProUGUI>();
-           value = ValuePanel.GetComponentsInChildren<TextMeshProUGUI>();
-           button = transform.GetComponentsInChildren<Button>();
+    void Start()
+    {
+        Statname = NamePanel.GetComponentsInChildren<TextMeshProUGUI>();
+        Statvalue = ValuePanel.GetComponentsInChildren<TextMeshProUGUI>();
+        button = transform.GetComponentsInChildren<Button>();
+    }
 
-       }
+    // Update is called once per frame
+    void Update()
+    {
 
-       // Update is called once per frame
-       void Update()
-       {
-
-       }*/
+    }
     #endregion
-    public TextMeshProUGUI STR;
+
+    #region Test1
+    /*public TextMeshProUGUI STR;
     public TextMeshProUGUI DEX;
     public TextMeshProUGUI INT;
     public TextMeshProUGUI VATAL;
@@ -38,10 +39,11 @@ public class TestStatUP : MonoBehaviour
     public Button button4;
 
     public PlayerData playerData = CharacterManager.PlayerCharacterData;
-    
 
     private void Start()
     {
+        playerData = CharacterManager.PlayerCharacterData;
+
         STR.text = playerData.strength.ToString();
         DEX.text = playerData.agility.ToString();
         INT.text = playerData.intelligence.ToString();
@@ -59,9 +61,8 @@ public class TestStatUP : MonoBehaviour
     {
         playerData.UpgradeStat(statType);
         Debug.Log("버튼 클릭");
-    }
-
-
+    }*/
+    #endregion
 }
 
 
