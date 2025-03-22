8311848b2114ec4dfc508aabbc099619c7e9b0f5
UnderBridgeKid97 <waktaverse24@gmail.com>
2025-03-22 22:25:58 +0900

    03.22
    중간저장

--- Assets/_WorkPlace/Chan/UI Script/StatusUI.cs
+++ Assets/_WorkPlace/Chan/UI Script/StatusUI.cs
@@ -1,118 +1,198 @@
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-/// <summary>
-/// 03.20 스테이터스ui 새로 작성중
-/// </summary>
+
 public class StatusUI : MonoBehaviour
 {
-    private enum CharType { Player, Dragon } // 카테고리 이넘 
+    private enum CharType { Player, Dragon }
     private CharType characterType;
 
-    [SerializeField] private GameObject defaultPanel;// 공용 네임, 레벨, 경험치 판넬
-    [SerializeField] private GameObject playerPanel; // 플레이어 스탯판넬 / 버튼
-    [SerializeField] private GameObject dragonbPanel;// 드래곤 스탯판넬
+    [Header("공통 패널")]
+    [SerializeField] private GameObject defaultPanel;
+    private TextMeshProUGUI[] DefaultText;
+    private Image[] DefaultImages;
 
-    private Dictionary<StatType, (Button plus, Button minus)> statButtons = new Dictionary<StatType, (Button, Button)>();
-    PlayerData playerData; 
-    DragonData dragonData;
+    [Header("플레이어 패널")]
+    [SerializeField] private GameObject playerPanel;
+    [SerializeField] private Transform statValuePosition;
+    [SerializeField] private Transform statPreviewPosition;
+    private TextMeshProUGUI[] playerStatTexts;
+    private TextMeshProUGUI[] previewStatTexts;
 
-    private TextMeshProUGUI[] PlayerStats;
+    [Header("드래곤 패널")]
+    [SerializeField] private GameObject dragonPanel;
     private TextMeshProUGUI[] DragonStats;
-    private TextMeshProUGUI[] DefaultText; // 공용 판넬
-    private Image[] DeFaultImage;
-    [SerializeField] private Button confirmButton; // 확정 버튼
 
+    [Header("버튼 영역")]
+
+    private Button[] buttons;
+    private Button playerTabButton;
+    private Button dragonTabButton;
+    private Button confirmButton;
+    private Dictionary<StatType, (Button plus, Button minus)> statButtons = new();
+
+    private PlayerData playerData;
+    private DragonData dragonData;
 
     private void Awake()
     {
-        // CharacterManager에서 PlayerCharacterData 가져오기
         playerData = CharacterManager.PlayerCharacterData;
         dragonData = CharacterManager.DragonData;
-        if (playerData == null)
-        {
-            Debug.LogError("플레이어 데이터가 없음");
-        }
-        if (dragonData == null)
-        {
-            Debug.LogError("드래곤 데이터가 없음");
-        }
 
-        PlayerStats = playerPanel.GetComponentsInChildren<TextMeshProUGUI>();
-        DragonStats = dragonbPanel.GetComponentsInChildren<TextMeshProUGUI>();
-
-        DeFaultImage = defaultPanel.GetComponentsInChildren<Image>();
         DefaultText = defaultPanel.GetComponentsInChildren<TextMeshProUGUI>();
+        DefaultImages = defaultPanel.GetComponentsInChildren<Image>();
+        playerStatTexts = statValuePosition.GetComponentsInChildren<TextMeshProUGUI>();
+        previewStatTexts = statPreviewPosition.GetComponentsInChildren<TextMeshProUGUI>();
+        DragonStats = dragonPanel.GetComponentsInChildren<TextMeshProUGUI>();
 
-     Button[] allbuttons = playerPanel.GetComponentsInChildren<Button>();
-        foreach(Button btn in allbuttons)
-        {
-            switch(btn.name)
-            {
-                case"UP0":
-                    if (!statButtons.ContainsKey(StatType.Strength)) statButtons[StatType.Strength] = (btn, null);
-                    else statButtons[StatType.Strength] = (btn, statButtons[StatType.Strength].plus);
-                    break;
-                case "UP1":
-                    if (!statButtons.ContainsKey(StatType.Intelligence)) statButtons[StatType.Intelligence] = (btn, null);
-                    else statButtons[StatType.Intelligence] = (btn, statButtons[StatType.Intelligence].plus);
-                    break;
-                case "UP2":
-                    if (!statButtons.ContainsKey(StatType.Agility)) statButtons[StatType.Agility] = (btn, null);
-                    else statButtons[StatType.Agility] = (btn, statButtons[StatType.Agility].plus);
-                    break;
-                case "UP3":
-                    if (!statButtons.ContainsKey(StatType.Vitality)) statButtons[StatType.Vitality] = (btn, null);
-                    else statButtons[StatType.Vitality] = (btn, statButtons[StatType.Vitality].plus);
-                    break;
-                //=================================================================================================== 
-                case "Down0":
-                    if (!statButtons.ContainsKey(StatType.Strength)) statButtons[StatType.Strength] = (btn, null);
-                    else statButtons[StatType.Strength] = (btn, statButtons[StatType.Strength].minus);
-                    break;
-                case "Down1":
-                    if (!statButtons.ContainsKey(StatType.Intelligence)) statButtons[StatType.Intelligence] = (btn, null);
-                    else statButtons[StatType.Intelligence] = (btn, statButtons[StatType.Intelligence].minus);
-                    break;
-                case "Down2":
-                    if (!statButtons.ContainsKey(StatType.Agility)) statButtons[StatType.Agility] = (btn, null);
-                    else statButtons[StatType.Agility] = (btn, statButtons[StatType.Agility].minus);
-                    break;
-                case "Down3":
-                    if (!statButtons.ContainsKey(StatType.Vitality)) statButtons[StatType.Vitality] = (btn, null);
-                    else statButtons[StatType.Vitality] = (btn, statButtons[StatType.Vitality].minus);
-                    break;
-                case "Confirm":
-                    confirmButton = btn;
-                    break;
-            }
-        }
-
+        AssignButtonsByIndex();
     }
 
     private void OnEnable()
     {
-        ShowPlayerStats(); // 기본적으로 플레이어 패널 활성화
-        // AddButtonListenrs();
+        playerData.CancelTempAllocation();
+        OnTabClick(CharType.Player);
+        AddButtonListeners();
+       
     }
 
     private void OnDisable()
     {
-     //   RemoveButtonListeners();
+        RemoveButtonListeners();
     }
+
+    private void AssignButtonsByIndex()
+    {
+        buttons = transform.GetComponentsInChildren<Button>(true);
+        if (buttons.Length < 11)
+        {
+            Debug.LogError("버튼 개수가 부족합니다. 총 11개 필요.");
+            return;
+        }
+
+        playerTabButton = buttons[0];
+        dragonTabButton = buttons[1];
+        confirmButton = buttons[10];
+
+        statButtons[StatType.Strength] = (buttons[2], buttons[6]);
+        statButtons[StatType.Intelligence] = (buttons[3], buttons[7]);
+        statButtons[StatType.Agility] = (buttons[4], buttons[8]);
+        statButtons[StatType.Vitality] = (buttons[5], buttons[9]);
+    }
+
+    private void AddButtonListeners()
+    {
+        Debug.Log("[AddButtonListeners 호출됨]");
+        playerTabButton?.onClick.AddListener(() => OnTabClick(CharType.Player));
+        dragonTabButton?.onClick.AddListener(() => OnTabClick(CharType.Dragon));
+        confirmButton?.onClick.AddListener(OnClickConfirm);
+
+        foreach (var pair in statButtons)
+        {
+            pair.Value.plus?.onClick.AddListener(() => OnClickStatButton(pair.Key, 1));
+            pair.Value.minus?.onClick.AddListener(() => OnClickStatButton(pair.Key, -1));
+        }
+    }
+
+    private void RemoveButtonListeners()
+    {
+        playerTabButton?.onClick.RemoveAllListeners();
+        dragonTabButton?.onClick.RemoveAllListeners();
+        confirmButton?.onClick.RemoveAllListeners();
+
+        foreach (var pair in statButtons)
+        {
+            pair.Value.plus?.onClick.RemoveAllListeners();
+            pair.Value.minus?.onClick.RemoveAllListeners();
+        }
+    }
+
+    private void OnTabClick(CharType type)
+    {
+        characterType = type;
+        UpdateUI();
+    }
+
+    private void OnClickStatButton(StatType stat, int delta)
+    {
+        Debug.Log("버튼 눌림");
+        if (!playerData.AdjustTempStat(stat, delta)) return;
+        UpdateUI();
+    }
+
+    private void OnClickConfirm()
+    {
+        Debug.Log("버튼 눌림");
+        playerData.ConfirmTempAllocation();
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        switch (characterType)
+        {
+            case CharType.Player:
+                ShowPlayerStats();
+                break;
+            case CharType.Dragon:
+                ShowDragonStats();
+                break;
+        }
+    }
+
     private void ShowPlayerStats()
     {
         playerPanel.SetActive(true);
-        dragonbPanel.SetActive(false);
-       // UpdateCommonPanel(CharType.Player);
+        dragonPanel.SetActive(false);
+
+      /*  DefaultText[0].text = playerData.characterName;
+        DefaultText[1].text = $"Lv. {playerData.level}";
+        DefaultText[2].text = $"EXP {playerData.currentExperience} / {playerData.experienceToLevelUp}";
+        DefaultImages[0].fillAmount = (float)playerData.currentExperience / playerData.experienceToLevelUp;*/
+
+        UpdatePlayerStatUI(StatType.Strength, 0);
+        UpdatePlayerStatUI(StatType.Intelligence, 1);
+        UpdatePlayerStatUI(StatType.Agility, 2);
+        UpdatePlayerStatUI(StatType.Vitality, 3);
     }
 
     private void ShowDragonStats()
     {
         playerPanel.SetActive(false);
-        dragonbPanel.SetActive(true);
-     //   UpdateCommonPanel(CharType.Dragon);
+        dragonPanel.SetActive(true);
+
+     /*   DefaultText[0].text = dragonData.characterName;
+        DefaultText[1].text = $"Lv. {dragonData.bondLevel}";
+        DefaultText[2].text = $"유대감 {dragonData.bondExperience} / {dragonData.CalculateExperienceToLevelUp(dragonData.bondLevel)}";
+        DefaultImages[0].fillAmount = (float)dragonData.bondExperience / dragonData.CalculateExperienceToLevelUp(dragonData.bondLevel);*/
+
+        DragonStats[0].text = dragonData.strength.ToString();
+        DragonStats[1].text = dragonData.intelligence.ToString();
+        DragonStats[2].text = dragonData.agility.ToString();
+        DragonStats[3].text = dragonData.vitality.ToString();
+    }
+
+    private void UpdatePlayerStatUI(StatType stat, int index)
+    {
+        int baseValue = GetStatByType(stat); // 현재 실제값
+        int preview = playerData.GetPreviewStat(stat); // 임시 포함값
+
+        playerStatTexts[index].text = baseValue.ToString();
+        previewStatTexts[index].text = preview.ToString();
+
+
+    }
+
+    private int GetStatByType(StatType stat)
+    {
+        return stat switch
+        {
+            StatType.Strength => playerData.strength,
+            StatType.Intelligence => playerData.intelligence,
+            StatType.Agility => playerData.agility,
+            StatType.Vitality => playerData.vitality,
+            _ => 0
+        };
     }
 }
